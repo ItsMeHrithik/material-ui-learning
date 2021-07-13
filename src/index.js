8c5a4c7f1ef551fc1b4b1092bd49566332ad5ec9
hrithik_bt18 <hrithik_bt18@iiitkalyani.ac.in>
2021-07-13 14:39:38 +0530

    created the react project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App'
+
+const MaterialUIApp = () => {
+  return <App />;
+};
+
+ReactDOM.render(<MaterialUIApp />, document.getElementById("root"));
